f7baf1db63aaa2664df55976407d5a46c7aa471c
anjelikamaulida <145630663+anjelikamaulida@users.noreply.github.com>
2023-12-14 07:15:46 +0700

    Create footer.php by Anjelika

---
+++ application/views/templates/footer.php
@@ -0,0 +1,48 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#products">Products</a>
+            <a href="#review">Review</a>
+            <a href="#contact">Contact</a>
+        </div>
+
+        <div class="box">
+            <h3>Social Media</h3>
+            <a>Instagram : @miracleflorist</a>
+            <a>Facebook: Official Miracle Florist</a>
+            <a>Pinterest : Official Miracle Florist</a>
+        </div>
+
+        <div class="box">
+            <h3>E-Commerce</h3>
+            <a>Shopee : @miracleflorist</a>
+            <a>Tokopedia: @miracleflorist</a>
+            <a>Lazada : @miracleflorist</a>
+        </div>
+
+        <div class="box">
+            <h3>Locations</h3>
+            <a>Jakarta</a>
+            <a>Bogor</a>
+            <a>Bandung</a>
+            <a>Depok</a>
+            <a>Tangerang</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact Info</h3>
+            <a>+6288889997777</a>
+            <a>miracleflorist@gmail.com</a>
+            <a>Bogor, Indonesia - 122720</a>
+        </div>
+    </div>
+
+    <div class="credit"> © <span> Miracle Florist </span> | All Right Reserved </div>
+
+</section></body>
+</html>
